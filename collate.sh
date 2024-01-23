df0669cdf19cb3011b0ac2a17b9e806fd96bbb70
Johannes Ahlmann <johannes@fluquid.com>
2024-01-23 10:14:31 +0000

    generated pdfs from .ipynb files and collated pdfs into topics

--- collate.sh
+++ collate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # collate documents into weeks/ topics
+mkdir collated_pdf
 for section in $(ls -1 dl_class/lectures/)
 do
-	qpdf --empty --pages $(ls -1 dl_class/lectures/${section}/*.pdf | tr '\n' ' ') -- "./${section}.pdf"
+	qpdf --empty --pages $(ls -1 dl_class/lectures/${section}/*.pdf | tr '\n' ' ') -- "collated_pdf/${section}.pdf"
 done
--- fetch_materials.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# download class materials from philkr.net
-wget -r --level=1 "http://www.philkr.net/dl_class/material" --wait=5 --no-host-directories
